a7cceaaf096d7e00d924f05fe5628b8dc16ba37d
Gabriel Abrao <gguimaraesabrao@gmail.com>
2024-07-02 17:19:03 -0300

    Organize routes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
-Route::view('/cart', 'cart')->middleware(['auth', 'verified'])->name('cart');
-
-Route::resource('products', ProductController::class)->middleware(['auth']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [ProductController::class, 'index'])->name('dashboard');
+    Route::view('/cart', 'cart')->name('cart');
+    Route::resource('products', ProductController::class)->except('index');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
