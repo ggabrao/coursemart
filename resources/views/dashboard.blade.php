032442b2662778ff23156fbc3fb03d7f9634c538
Gabriel Abrao <gguimaraesabrao@gmail.com>
2024-07-03 16:59:02 -0300

    Improve UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,10 +22,21 @@
                         @forelse($products as $product)
 
                             <div
-                                class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                class="w-full h-[500px] max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                 <div class="flex justify-between px-4 pt-4">
-                                    <div><span
-                                            class="text-sm text-gray-500 dark:text-gray-400">Seller id: {{$product->user_id}}</span>
+                                    @if($product->user_id === Auth::id())
+                                        <div
+                                            class="p-1 text-msd text-green-800 rounded-md bg-green-200 dark:bg-gray-800 dark:text-yellow-300 text-center"
+                                        >
+                                            <span class="font-bold">{{ __('Your product') }}</span>
+                                        </div>
+                                    @else()
+                                        <span
+                                            class="text-sm text-gray-500 dark:text-gray-400">Seller Id: {{$product->user_id}}
+                                        </span>
+                                    @endif
+                                    <div>
+
                                     </div>
                                     <button id="{{$product -> id}}-dropdownButton"
                                             data-dropdown-toggle="{{$product -> id}}-dropdown"
@@ -63,7 +74,7 @@
                                 </div>
 
 
-                                <div class="flex flex-col items-center pb-10">
+                                <div class="flex flex-col items-center pb-10 mt-4">
 
                                     <h5 class="mb-1 text-3xl font-extrabold text-gray-900 dark:text-white">{{$product->name}}</h5>
                                     <span
@@ -75,54 +86,57 @@
 
                                     <div class="flex flex-col mt-4 md:mt-10">
                                         <form class="max-w-xs mx-auto">
-                                            <label for="{{$product -> id}}-counter-input"
-                                                   class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Select
-                                                a
-                                                quantity:</label>
-                                            <div class="relative flex items-center justify-center">
-                                                <button type="button" id="{{$product -> id}}-decrement-button"
-                                                        data-input-counter-decrement="{{$product -> id}}-counter-input"
-                                                        class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none justify-center">
-                                                    <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white"
-                                                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                         fill="none" viewBox="0 0 18 2">
-                                                        <path stroke="currentColor" stroke-linecap="round"
-                                                              stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
-                                                    </svg>
-                                                </button>
-                                                <input type="text" id="{{$product -> id}}-counter-input"
-                                                       data-input-counter
-                                                       class="flex-shrink-0 text-gray-900 dark:text-white border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-[2.5rem] text-center"
-                                                       value="1" required/>
-                                                <button type="button" id="{{$product -> id}}-increment-button"
-                                                        data-input-counter-increment="{{$product -> id}}-counter-input"
-                                                        class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
-                                                    <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white"
-                                                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                         fill="none" viewBox="0 0 18 18">
-                                                        <path stroke="currentColor" stroke-linecap="round"
-                                                              stroke-linejoin="round" stroke-width="2"
-                                                              d="M9 1v16M1 9h16"/>
-                                                    </svg>
-                                                </button>
-                                            </div>
-                                            <button type="submit"
-                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-3">
-                                                Add to cart
-                                            </button>
+                                            @unless($product->user_id === Auth::id())
+                                                <label for="{{$product -> id}}-counter-input"
+                                                       class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Select
+                                                    a
+                                                    quantity:</label>
 
+                                                <div class="relative flex items-center justify-center">
+
+                                                    <button type="button" id="{{$product -> id}}-decrement-button"
+                                                            data-input-counter-decrement="{{$product -> id}}-counter-input"
+                                                            class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none justify-center">
+                                                        <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white"
+                                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                             fill="none" viewBox="0 0 18 2">
+                                                            <path stroke="currentColor" stroke-linecap="round"
+                                                                  stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
+                                                        </svg>
+                                                    </button>
+                                                    <input type="text" id="{{$product -> id}}-counter-input"
+                                                           data-input-counter
+                                                           class="flex-shrink-0 text-gray-900 dark:text-white border-0 bg-transparent text-sm font-normal focus:outline-none focus:ring-0 max-w-[2.5rem] text-center"
+                                                           value="1" required/>
+                                                    <button type="button" id="{{$product -> id}}-increment-button"
+                                                            data-input-counter-increment="{{$product -> id}}-counter-input"
+                                                            class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                        <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white"
+                                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                             fill="none" viewBox="0 0 18 18">
+                                                            <path stroke="currentColor" stroke-linecap="round"
+                                                                  stroke-linejoin="round" stroke-width="2"
+                                                                  d="M9 1v16M1 9h16"/>
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                                <button type="submit"
+                                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-3">
+                                                    Add to cart
+                                                </button>
+                                            @endunless
                                         </form>
 
                                     </div>
                                 </div>
 
-                                @unless ($product->created_at->eq($product->updated_at))
+                                @if ($product->created_at->eq($product->updated_at) and $product->user_id !== Auth::id())
                                     <div
                                         class="p-2 mb-4 text-md text-yellow-800 rounded-sm bg-yellow-200 dark:bg-gray-800 dark:text-yellow-300 text-center"
                                     >
                                         <span class="font-extrabold">{{ __('Recently updated!') }}</span>
                                     </div>
-                                @endunless
+                                @endif
 
                             </div>
 
