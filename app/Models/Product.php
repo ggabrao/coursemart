10a047767ea7b3e781bd4e9f0fe4c55187897baf
Gabriel Abrao <gguimaraesabrao@gmail.com>
2024-07-08 17:48:21 -0300

    Revert "Add database configs for cart system"
    This reverts commit 13ad77d584730f4d95f1176466f1386ef89ff400.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -21,11 +20,6 @@ class Product extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function cart_items(): BelongsToMany
-    {
-        return $this->belongsToMany(CartItem::class);
     }
 
 }
--- database/migrations/2024_07_02_170011_create_products_table.php
+++ database/migrations/2024_07_02_170011_create_products_table.php
@@ -19,28 +19,6 @@ return new class extends Migration {
             $table->decimal('price');
             $table->timestamps();
         });
-
-        Schema::create('carts', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('cart_item_id');
-            $table->decimal('total');
-            $table->timestamps();
-        });
-
-        Schema::create('cartItems', function (Blueprint $table) {
-            $table->id();
-            $table->integer('quantity');
-            $table->decimal('total');
-            $table->timestamps();
-        });
-
-        Schema::create('cartItems_products', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('cart_item_id')->constrained('cart_items')->cascadeOnDelete();
-            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
-            $table->timestamps();
-        });
     }
 
     /**
