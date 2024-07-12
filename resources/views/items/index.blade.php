0ceba6df701b3d58042b8444644bfeaeb69e8557
Gabriel Abrao <gguimaraesabrao@gmail.com>
2024-07-12 14:25:16 -0300

    Fix

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -12,43 +12,44 @@
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        @foreach($items as $item)
-                            <tr>
-                                <th scope="col" class="px-16 py-3">
-                                    Product
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Qty
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Price
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Subtotal
-                                </th>
-                                <th scope="col" class="px-6 py-3">
 
-                                </th>
-                            </tr>
+                        <tr>
+                            <th scope="col" class="px-16 py-3">
+                                Product
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Qty
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Subtotal
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+
+                            </th>
+                        </tr>
                         </thead>
                         <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="px-16 py-4 font-semibold text-gray-900 dark:text-white">
-                                {{$item->products->value('name')}}
-                            </td>
+                        @foreach($items as $item)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td class="px-16 py-4 font-semibold text-gray-900 dark:text-white">
+                                    {{$item->products->value('name')}}
+                                </td>
 
-                            <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                {{$item->quantity}}
-                            </td>
+                                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                    {{$item->quantity}}
+                                </td>
 
-                            <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                $ {{number_format($item->products->value('price'), 2)}}
-                            </td>
-                            <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                                ${{ number_format(($item->quantity) * $item->products->value('price'), 2)}}
-                            </td>
+                                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                    $ {{number_format($item->products->value('price'), 2)}}
+                                </td>
+                                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                                    ${{ number_format(($item->quantity) * $item->products->value('price'), 2)}}
+                                </td>
 
-                        </tr>
+                            </tr>
                         @endforeach
 
 
